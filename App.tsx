79a14664f6c233c151d8604a36b09ede0b561f35
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2022-12-29 17:43:19 -0300

    feat: project structure
    estrutura da aplicacao com native base

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StatusBar, Text, View } from 'react-native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { NativeBaseProvider } from 'native-base';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
+
+  return (
+    <NativeBaseProvider>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />      
+      {fontsLoaded ? <Text>Hello!</Text> : <View />}
+    </NativeBaseProvider>
+  );
+}
