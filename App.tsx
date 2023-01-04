1626e9429095ada6255c9f55dc6e744b73eb3731
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-01-04 09:00:13 -0300

    feat: Signin page
    Tela de signin com background

--- App.tsx
+++ App.tsx
@@ -1,22 +1,30 @@
-import React from 'react';
-import { StatusBar, Text, View } from 'react-native';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { NativeBaseProvider } from 'native-base';
+import { StatusBar } from 'react-native';
+
+import { Loading } from '@components/Loading';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+import { SignIn } from '@screens/SignIn';
+
+import { THEME } from './src/theme';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
-    Roboto_700Bold
-  })
+    Roboto_700Bold,
+  });
 
   return (
-    <NativeBaseProvider>
-      <StatusBar 
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
-      />      
-      {fontsLoaded ? <Text>Hello!</Text> : <View />}
+      />
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,25 @@
+import { Center, Image, Text, VStack } from 'native-base';
+
+import BackgroundImg from '@assets/background.png';
+import LogoSvg from '@assets/logo.svg';
+
+export function SignIn() {
+  return (
+    <VStack flex={1} bg="gray.700">
+      <Image
+        source={BackgroundImg}
+        alt="Pessoas treinando"
+        resizeMethod="auto"
+        position="absolute"
+      />
+
+      <Center my={24}>
+        <LogoSvg />
+
+        <Text color="gray.100" fontSize="sm">
+          Treine sua mente e o seu corpo
+        </Text>
+      </Center>
+    </VStack>
+  );
+}
