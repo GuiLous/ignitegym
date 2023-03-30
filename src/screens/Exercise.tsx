5c5930b01bbc4a0c4df4154178c46b1335315858
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-03-30 09:53:49 -0300

    feat: registrando historico

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -4,6 +4,7 @@ import BodySvg from '@assets/body.svg';
 import RepetitionsSvg from '@assets/repetitions.svg';
 import SeriesSvg from '@assets/series.svg';
 import { Button } from '@components/Button';
+import { Loading } from '@components/Loading';
 import { ExerciseDTO } from '@dtos/ExerciseDTO';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation, useRoute } from '@react-navigation/native';
@@ -27,10 +28,11 @@ type ExerciseRouteParamsProps = {
 };
 
 export function Exercise() {
+  const [submittingRegister, setSubmittingRegister] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [exercise, setExercise] = useState<ExerciseDTO>({} as ExerciseDTO);
-  console.log('🚀 - exercise:', exercise);
 
-  const { goBack } = useNavigation<AppNavigatorRoutesProps>();
+  const { goBack, navigate } = useNavigation<AppNavigatorRoutesProps>();
 
   const toast = useToast();
   const route = useRoute();
@@ -43,6 +45,7 @@ export function Exercise() {
 
   async function fetchExerciseDetails() {
     try {
+      setIsLoading(true);
       const response = await api.get(`/exercises/${exerciseId}`);
       setExercise(response.data);
     } catch (error) {
@@ -56,6 +59,37 @@ export function Exercise() {
         placement: 'top',
         bgColor: 'red.500',
       });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function handleExerciseHistoryRegister() {
+    try {
+      setSubmittingRegister(true);
+
+      await api.post('/history', { exercise_id: exerciseId });
+
+      toast.show({
+        title: 'Parabens! Exercicio registrado.',
+        placement: 'top',
+        bgColor: 'green.700',
+      });
+
+      navigate('history');
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError
+        ? error.message
+        : 'Não foi possível registrar o exercício.';
+
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    } finally {
+      setSubmittingRegister(false);
     }
   }
 
@@ -93,44 +127,52 @@ export function Exercise() {
       </VStack>
 
       <ScrollView>
-        <VStack p={8}>
-          <Box mb={3} rounded="lg" overflow="hidden">
-            <Image
-              w="full"
-              h={80}
-              source={{
-                uri: `${api.defaults.baseURL}/exercise/demo/${exercise?.demo}`,
-              }}
-              alt="Nome do exercício"
-              resizeMode="cover"
-              rounded="lg"
-            />
-          </Box>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <VStack p={8}>
+            <Box mb={3} rounded="lg" overflow="hidden">
+              <Image
+                w="full"
+                h={80}
+                source={{
+                  uri: `${api.defaults.baseURL}/exercise/demo/${exercise?.demo}`,
+                }}
+                alt="Nome do exercício"
+                resizeMode="cover"
+                rounded="lg"
+              />
+            </Box>
 
-          <Box bg="gray.600" rounded="md" pb={4} px={4}>
-            <HStack
-              alignItems="center"
-              justifyContent="space-around"
-              mb={6}
-              mt={5}>
-              <HStack>
-                <SeriesSvg />
-                <Text color="gray.200" ml="2">
-                  {exercise.series} séries
-                </Text>
+            <Box bg="gray.600" rounded="md" pb={4} px={4}>
+              <HStack
+                alignItems="center"
+                justifyContent="space-around"
+                mb={6}
+                mt={5}>
+                <HStack>
+                  <SeriesSvg />
+                  <Text color="gray.200" ml="2">
+                    {exercise.series} séries
+                  </Text>
+                </HStack>
+
+                <HStack>
+                  <RepetitionsSvg />
+                  <Text color="gray.200" ml="2">
+                    {exercise.repetitions} repetições
+                  </Text>
+                </HStack>
               </HStack>
 
-              <HStack>
-                <RepetitionsSvg />
-                <Text color="gray.200" ml="2">
-                  {exercise.repetitions} repetições
-                </Text>
-              </HStack>
-            </HStack>
-
-            <Button title="Marcar como realizado" />
-          </Box>
-        </VStack>
+              <Button
+                title="Marcar como realizado"
+                isLoading={submittingRegister}
+                onPress={handleExerciseHistoryRegister}
+              />
+            </Box>
+          </VStack>
+        )}
       </ScrollView>
     </VStack>
   );
