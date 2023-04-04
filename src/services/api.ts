7350bfebb3fdf92e9e69d3e83463c4a0ef62a143
GuilhermeLou22 <guilhermesilva@acad.ifma.edu.br>
2023-04-04 09:51:50 -0300

    feat: obtendo refresh_token
    Salvando o refresh_token no storage e buscando

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,19 +1,48 @@
+import { storageAuthTokenGet } from '@storage/storageAuthToken';
 import { AppError } from '@utils/AppError';
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
+
+type SignOut = () => void;
+
+type APIIstanceProps = AxiosInstance & {
+  registerInterceptTokenManager: (SignOut: SignOut) => () => void;
+};
 
 const api = axios.create({
   baseURL: 'http://192.168.1.115:3333',
-});
+}) as APIIstanceProps;
 
-api.interceptors.response.use(
-  response => response,
-  error => {
-    if (error.response && error.response.data) {
-      return Promise.reject(new AppError(error.response.data.message));
-    } else {
-      return Promise.reject(error);
-    }
-  },
-);
+api.registerInterceptTokenManager = SignOut => {
+  const interceptTokenManager = api.interceptors.response.use(
+    response => response,
+    async requestError => {
+      if (requestError?.response?.status === 401) {
+        if (
+          requestError.response.data?.message === 'token.expired' ||
+          requestError.response.data?.message === 'token.invalid'
+        ) {
+          const { refresh_token } = await storageAuthTokenGet();
+
+          if (!refresh_token) {
+            SignOut();
+            return Promise.reject(requestError);
+          }
+        }
+
+        SignOut();
+      }
+
+      if (requestError.response && requestError.response.data) {
+        return Promise.reject(new AppError(requestError.response.data.message));
+      } else {
+        return Promise.reject(requestError);
+      }
+    },
+  );
+
+  return () => {
+    api.interceptors.response.eject(interceptTokenManager);
+  };
+};
 
 export { api };
